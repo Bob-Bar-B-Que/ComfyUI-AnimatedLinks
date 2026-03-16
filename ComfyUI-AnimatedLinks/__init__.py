73a389e864905da6236b8a10458ac078f9df5f0c
Bob-Bar-B-Que <70482480+Bob-Bar-B-Que@users.noreply.github.com>
2026-03-16 23:03:47 +0200

    Delete ComfyUI-AnimatedLinks directory
    didn't upload it properly

--- ComfyUI-AnimatedLinks/__init__.py
+++
@@ -1,8 +0,0 @@
-# ComfyUI-AnimatedLinks
-# Animated links and Set/Get node highlighting for ComfyUI
-# Author: Bob-Bar-B-Que
-# License: MIT
-
-NODE_CLASS_MAPPINGS = {}
-NODE_DISPLAY_NAME_MAPPINGS = {}
-WEB_DIRECTORY = "./web"
